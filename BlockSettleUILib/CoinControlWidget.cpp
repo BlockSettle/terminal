b9e973d322092a030549ef9a419ae7bb3192af60
Igor Mironchik <igor.mironchik@gmail.com>
2018-06-13 12:50:18 +0300

    Enabled checking/unchecking of checkboxes by clicking any place on the row in coin control widget.

--- BlockSettleUILib/CoinControlWidget.cpp
+++ BlockSettleUILib/CoinControlWidget.cpp
@@ -5,6 +5,102 @@
 #include "CoinControlModel.h"
 #include "SelectedTransactionInputs.h"
 
+#include <QTreeView>
+#include <QStyledItemDelegate>
+#include <QStyle>
+#include <QApplication>
+#include <QStyleOptionViewItem>
+
+
+//
+// DelegateFor0Column
+//
+
+//! Delegate for the first (0) column.
+class DelegateFor0Column Q_DECL_FINAL : public QStyledItemDelegate
+{
+public:
+   explicit DelegateFor0Column(QObject *parent)
+      : QStyledItemDelegate(parent)
+      , mouseButtonPressed_(false)
+   {
+   }
+
+   virtual ~DelegateFor0Column()
+   {
+   }
+
+
+   bool editorEvent(QEvent *event, QAbstractItemModel *model, const QStyleOptionViewItem &option,
+                    const QModelIndex &index) Q_DECL_OVERRIDE
+   {
+      if (index.isValid()) {
+         switch (event->type()) {
+            case QEvent::MouseButtonPress :
+            {
+               QMouseEvent *e = static_cast<QMouseEvent*>(event);
+
+               if (e->button() == Qt::LeftButton) {
+                  const QPoint pos = e->pos();
+
+                  QRect textRect = QApplication::style()->subElementRect(QStyle::SE_ItemViewItemText,
+                                                                          &option);
+
+                  if (textRect.contains(pos)) {
+                     mouseButtonPressed_ = true;
+
+                     return false;
+                  } else {
+                     return QStyledItemDelegate::editorEvent(event, model, option, index);
+                  }
+               } else {
+                  return QStyledItemDelegate::editorEvent(event, model, option, index);
+               }
+            }
+               break;
+
+            case QEvent::MouseButtonRelease :
+            {
+               QMouseEvent *e = static_cast<QMouseEvent*>(event);
+
+               if (e->button() == Qt::LeftButton) {
+                  const QPoint pos = e->pos();
+
+                  QRect textRect = QApplication::style()->subElementRect(QStyle::SE_ItemViewItemText,
+                                                                          &option);
+
+                  if (textRect.contains(pos) && mouseButtonPressed_) {
+                     mouseButtonPressed_ = false;
+
+                     const int currentState = model->data(index, Qt::CheckStateRole).toInt();
+                     model->setData(index, (currentState == Qt::Unchecked ?
+                                               Qt::Checked : Qt::Unchecked),
+                                    Qt::CheckStateRole);
+
+                     return false;
+                  } else {
+                     mouseButtonPressed_ = false;
+
+                     return QStyledItemDelegate::editorEvent(event, model, option, index);
+                  }
+               } else {
+                  return QStyledItemDelegate::editorEvent(event, model, option, index);
+               }
+            }
+               break;
+
+            default :
+               return QStyledItemDelegate::editorEvent(event, model, option, index);
+         }
+      } else {
+         return QStyledItemDelegate::editorEvent(event, model, option, index);
+      }
+   }
+
+private:
+   bool mouseButtonPressed_;
+}; // class DelegateFor0Column
+
 
 
 CoinControlWidget::CoinControlWidget(QWidget* parent)
@@ -36,6 +132,16 @@ void CoinControlWidget::onAutoSelClicked(int state)
    }
 }
 
+void CoinControlWidget::rowClicked(const QModelIndex &index)
+{
+   if (index.isValid() && index.column() != 0) {
+      const QModelIndex column0Index = coinControlModel_->index(index.row(), 0, index.parent());
+      const int currentState = coinControlModel_->data(column0Index, Qt::CheckStateRole).toInt();
+      coinControlModel_->setData(column0Index,
+         (currentState == Qt::Unchecked ? Qt::Checked : Qt::Unchecked), Qt::CheckStateRole);
+   }
+}
+
 void CoinControlWidget::initWidget(const std::shared_ptr<SelectedTransactionInputs>& selectedInputs)
 {
    assert(selectedInputs != nullptr);
@@ -43,6 +149,8 @@ void CoinControlWidget::initWidget(const std::shared_ptr<SelectedTransactionInpu
 
    coinControlModel_ = new CoinControlModel(selectedInputs);
    ui_->treeViewUTXO->setModel(coinControlModel_);
+
+   ui_->treeViewUTXO->setItemDelegateForColumn(0, new DelegateFor0Column(ui_->treeViewUTXO));
 
    auto ccHeader = new CCHeader(selectedInputs->GetTotalTransactionsCount(), Qt::Horizontal, ui_->treeViewUTXO);
    ccHeader->setStretchLastSection(true);
@@ -55,6 +163,8 @@ void CoinControlWidget::initWidget(const std::shared_ptr<SelectedTransactionInpu
    connect(coinControlModel_, &CoinControlModel::selectionChanged, this, &CoinControlWidget::updateSelectedTotals);
    connect(ui_->checkBoxUseAllSelected, &QCheckBox::stateChanged, this, &CoinControlWidget::onAutoSelClicked);
    onAutoSelClicked(selectedInputs->UseAutoSel() ? Qt::Checked : Qt::Unchecked);
+
+   connect(ui_->treeViewUTXO, &QTreeView::clicked, this, &CoinControlWidget::rowClicked);
 }
 
 void CoinControlWidget::applyChanges(const std::shared_ptr<SelectedTransactionInputs>& selectedInputs)
