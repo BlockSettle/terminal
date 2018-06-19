7cb291996de8b395f46189c0907846fed27fdf23
Igor Mironchik <igor.mironchik@gmail.com>
2018-06-19 19:01:08 +0300

    Fixed negative timer value in progress bar in the requesting quote widget.

--- BlockSettleUILib/RequestingQuoteWidget.cpp
+++ BlockSettleUILib/RequestingQuoteWidget.cpp
@@ -89,7 +89,8 @@ void RequestingQuoteWidget::ticker()
    }
    else {
       ui_->progressBar->setValue(timeDiff);
-      ui_->progressBar->setFormat(tr("%1 second(s) remaining").arg(QString::number(timeDiff/1000)));
+      ui_->progressBar->setFormat(tr("%1 second(s) remaining")
+         .arg(QString::number(timeDiff > 0 ? timeDiff/1000 : 0)));
    }
 }
 
