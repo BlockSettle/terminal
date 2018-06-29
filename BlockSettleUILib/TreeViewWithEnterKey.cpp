0c3bed78915e567c37cf5df9f91addb507b2b98e
Igor Mironchik <igor.mironchik@gmail.com>
2018-06-29 08:03:55 +0300

    Tree view with enter key pressed signal.
    Task-number: BST-1151

---
+++ BlockSettleUILib/TreeViewWithEnterKey.cpp
@@ -0,0 +1,28 @@
+
+
+#include <QKeyEvent>
+
+#include "TreeViewWithEnterKey.h"
+
+
+//
+// TreeViewWithEnterKey
+//
+
+TreeViewWithEnterKey::TreeViewWithEnterKey(QWidget *parent)
+   : QTreeView(parent)
+{
+}
+
+void TreeViewWithEnterKey::keyPressEvent(QKeyEvent *event)
+{
+   if (currentIndex().isValid()) {
+      if (event->key() == Qt::Key_Return || event->key() == Qt::Key_Enter) {
+         emit enterKeyPressed(currentIndex());
+
+         return;
+      }
+   }
+
+   QTreeView::keyPressEvent(event);
+}
