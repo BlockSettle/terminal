370426471a571521a584ef4b254c68944836d964
Sergey Chernikov <s.bilberry@gmail.com>
2018-07-11 14:32:58 +0300

    Additional lock for ZmqContext to prevent crashes at parallel socket creation

--- BlocksettleNetworkingLib/ZmqContext.cpp
+++ BlocksettleNetworkingLib/ZmqContext.cpp
@@ -3,14 +3,15 @@
 #include <zmq.h>
 #include <spdlog/spdlog.h>
 #include <stdexcept>
+#include "FastLock.h"
+
 
 ZmqContext::ZmqContext(const std::shared_ptr<spdlog::logger>& logger)
    : logger_(logger)
    , context_( nullptr, zmq_ctx_term )
 {
    context_.reset(zmq_ctx_new());
-   if (!context_)
-   {
+   if (!context_) {
       logger_->error("[ZmqContext] failed to initialize ZeroMQ context");
       throw std::runtime_error("Failed to init zeromq");
    }
@@ -18,25 +19,30 @@ ZmqContext::ZmqContext(const std::shared_ptr<spdlog::logger>& logger)
 
 ZmqContext::sock_ptr ZmqContext::CreateInternalControlSocket()
 {
+   FastLock lock(lockerFlag_);
    return { zmq_socket(context_.get(), ZMQ_PAIR), zmq_close };
 }
 
 ZmqContext::sock_ptr ZmqContext::CreateMonitorSocket()
 {
+   FastLock lock(lockerFlag_);
    return { zmq_socket(context_.get(), ZMQ_PAIR), zmq_close };
 }
 
 ZmqContext::sock_ptr ZmqContext::CreateStreamSocket()
 {
+   FastLock lock(lockerFlag_);
    return { zmq_socket(context_.get(), ZMQ_STREAM), zmq_close };
 }
 
 ZmqContext::sock_ptr    ZmqContext::CreateServerSocket()
 {
+   FastLock lock(lockerFlag_);
    return { zmq_socket(context_.get(), ZMQ_ROUTER), zmq_close };
 }
 ZmqContext::sock_ptr    ZmqContext::CreateClientSocket()
 {
+   FastLock lock(lockerFlag_);
    return { zmq_socket(context_.get(), ZMQ_DEALER), zmq_close };
 }
 
@@ -52,10 +58,12 @@ std::string ZmqContext::GenerateConnectionName(const std::string& host, const st
 
 ZmqContext::sock_ptr ZmqContext::CreatePublishSocket()
 {
+   FastLock lock(lockerFlag_);
    return { zmq_socket(context_.get(), ZMQ_PUB), zmq_close };
 }
 
 ZmqContext::sock_ptr ZmqContext::CreateSubscribeSocket()
 {
+   FastLock lock(lockerFlag_);
    return { zmq_socket(context_.get(), ZMQ_SUB), zmq_close };
 }
