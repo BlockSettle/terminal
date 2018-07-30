7a920d619f19e2abbc6e95a566ab1eec21654deb
Igor Mironchik <igor.mironchik@gmail.com>
2018-07-30 10:30:13 +0300

    Removed filtering settings from dealing page. State of collapsed/expanded groups in RFQ blotter saves in the application's settings.
    Task-number: BST-1227

--- BlockSettleUILib/DealingSettingsPage.cpp
+++ BlockSettleUILib/DealingSettingsPage.cpp
@@ -8,7 +8,6 @@
 DealingSettingsPage::DealingSettingsPage(QWidget* parent)
    : QWidget{parent}
    , ui_{new Ui::DealingSettingsPage{}}
-   , secModel_{nullptr}
 {
    ui_->setupUi(this);
 
@@ -24,28 +23,11 @@ void DealingSettingsPage::displaySettings(const std::shared_ptr<AssetManager> &a
    , bool displayDefault)
 {
    ui_->checkBoxDrop->setChecked(appSettings_->get<bool>(ApplicationSettings::dropQN, displayDefault));
-
-   if (assetMgr->hasSecurities()) {
-      secModel_ = new SecuritiesModel(assetMgr, appSettings_->get<QStringList>(ApplicationSettings::Filter_MD_QN)
-         , ui_->treeViewSecurities);
-      ui_->treeViewSecurities->setModel(secModel_);
-      secModel_->sort(0);
-      ui_->treeViewSecurities->expandAll();
-      ui_->treeViewSecurities->setHeaderHidden(true);
-      ui_->label->hide();
-   } else {
-      ui_->treeViewSecurities->hide();
-   }
 }
 
 void DealingSettingsPage::applyChanges()
 {
    appSettings_->set(ApplicationSettings::dropQN, ui_->checkBoxDrop->isChecked());
-
-   if (secModel_) {
-      const auto visibilitySettings = secModel_->getVisibilitySettings();
-      appSettings_->set(ApplicationSettings::Filter_MD_QN, visibilitySettings);
-   }
 }
 
 void DealingSettingsPage::onResetCountes()
