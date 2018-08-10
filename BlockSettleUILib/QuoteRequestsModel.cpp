5aceb56ae926b6cc2027498c3a76a8be13b0a35a
Igor Mironchik <igor.mironchik@gmail.com>
2018-08-10 12:08:08 +0300

    Optimizied updating of prices in RFQ blotter.
    Task-number: BST-1236

--- BlockSettleUILib/QuoteRequestsModel.cpp
+++ BlockSettleUILib/QuoteRequestsModel.cpp
@@ -690,7 +690,8 @@ void QuoteRequestsModel::onQuoteRejected(const QString &reqId, const QString &re
    setStatus(reqId.toStdString(), bs::network::QuoteReqNotification::Rejected, reason);
 }
 
-void QuoteRequestsModel::updateBestQuotePrice(const QString &reqId, double price, bool own)
+void QuoteRequestsModel::updateBestQuotePrice(const QString &reqId, double price, bool own,
+   std::vector<std::pair<QModelIndex, QModelIndex>> *idxs)
 {
    int row = -1;
    Group *g = nullptr;
@@ -710,10 +711,16 @@ void QuoteRequestsModel::updateBestQuotePrice(const QString &reqId, double price
    if (row >= 0 && g) {
       static const QVector<int> roles({static_cast<int>(Qt::DisplayRole),
          static_cast<int>(Qt::BackgroundRole)});
-      emit dataChanged(createIndex(row, static_cast<int>(Column::QuotedPx),
-            &g->rfqs_[static_cast<std::size_t>(row)]->idx_),
-         createIndex(row, static_cast<int>(Column::BestPx),
-            &g->rfqs_[static_cast<std::size_t>(row)]->idx_), roles);
+      const auto idx1 = createIndex(row, static_cast<int>(Column::QuotedPx),
+         &g->rfqs_[static_cast<std::size_t>(row)]->idx_);
+      const auto idx2 = createIndex(row, static_cast<int>(Column::BestPx),
+         &g->rfqs_[static_cast<std::size_t>(row)]->idx_);
+
+      if (!idxs) {
+         emit dataChanged(idx1, idx2, roles);
+      } else {
+         idxs->push_back(std::make_pair(idx1, idx2));
+      }
    }
 }
 
@@ -947,17 +954,54 @@ void QuoteRequestsModel::onDeferredUpdate(const QPersistentModelIndex &index)
 
 void QuoteRequestsModel::onPriceUpdateTimer()
 {
+   std::vector<std::pair<QModelIndex, QModelIndex>> idxs;
+
    for (auto it = prices_.cbegin(), last = prices_.cend(); it != last; ++it) {
-      updatePrices(it->first, it->second.first, it->second.second);
+      updatePrices(it->first, it->second.first, it->second.second, &idxs);
    }
 
    prices_.clear();
 
    for (auto it = bestQuotePrices_.cbegin(), last = bestQuotePrices_.cend(); it != last; ++it) {
-      updateBestQuotePrice(it->first, it->second.price_, it->second.own_);
+      updateBestQuotePrice(it->first, it->second.price_, it->second.own_, &idxs);
    }
 
    bestQuotePrices_.clear();
+
+   if (!idxs.empty()) {
+      std::map<QModelIndex, std::pair<std::pair<int, int>, std::pair<int, int>>> mapOfIdxs;
+
+      for (const auto &idx: idxs) {
+         auto it = mapOfIdxs.find(idx.first.parent());
+
+         if (it != mapOfIdxs.end()) {
+            if (idx.first.row() < it->second.first.first) {
+               it->second.first.first = idx.first.row();
+            }
+
+            if (idx.first.column() < it->second.first.second) {
+               it->second.first.second = idx.first.column();
+            }
+
+            if (idx.second.row() > it->second.second.first) {
+               it->second.second.first = idx.second.row();
+            }
+
+            if (idx.second.column() > it->second.second.second) {
+               it->second.second.second = idx.second.column();
+            }
+         } else {
+            mapOfIdxs[idx.first.parent()] = std::make_pair(
+               std::make_pair(idx.first.row(), idx.first.column()),
+               std::make_pair(idx.second.row(), idx.second.column()));
+         }
+      }
+
+      for (auto it = mapOfIdxs.cbegin(), last = mapOfIdxs.cend(); it != last; ++it) {
+         emit dataChanged(index(it->second.first.first, it->second.first.second, it->first),
+            index(it->second.second.first, it->second.second.second, it->first));
+      }
+   }
 }
 
 void QuoteRequestsModel::onSettlementExpired()
@@ -1119,9 +1163,9 @@ void QuoteRequestsModel::setStatus(const std::string &reqId, bs::network::QuoteR
 }
 
 void QuoteRequestsModel::updatePrices(const QString &security, const bs::network::MDField &pxBid,
-   const bs::network::MDField &pxOffer)
+   const bs::network::MDField &pxOffer, std::vector<std::pair<QModelIndex, QModelIndex>> *idxs)
 {
-   forEachSecurity(security, [security, pxBid, pxOffer, this](Group *grp, int index) {
+   forEachSecurity(security, [security, pxBid, pxOffer, this, idxs](Group *grp, int index) {
       const CurrencyPair cp(security.toStdString());
       const bool isBuy = (grp->rfqs_[static_cast<std::size_t>(index)]->side_ == bs::network::Side::Buy)
          ^ (cp.NumCurrency() == grp->rfqs_[static_cast<std::size_t>(index)]->product_.toStdString());
@@ -1151,7 +1195,12 @@ void QuoteRequestsModel::updatePrices(const QString &security, const bs::network
 
          const QModelIndex idx = createIndex(index, static_cast<int>(Column::IndicPx),
             &grp->rfqs_[static_cast<std::size_t>(index)]->idx_);
-         emit dataChanged(idx, idx);
+
+         if (!idxs) {
+            emit dataChanged(idx, idx);
+         } else {
+            idxs->push_back(std::make_pair(idx, idx));
+         }
       }
    });
 }
