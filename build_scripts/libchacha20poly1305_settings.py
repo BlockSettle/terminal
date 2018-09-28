0dc2b63c25b3baa2448b84fb8694ca745c972293
Douglas Roark <joroark@vt.edu>
2018-09-28 01:43:21 -0700

    Add BIP 150/151 support files
    Code is lifted from Armory and adds Jonas Schnelli's ChaCha20Poly1305 library to the 3rd party compilation process. Note that:
    
    - The code relies on libbtc (including libsecp256k1) and, as mentioned above, libchacha20poly1305. libbtc is already included in the BlockSettle 3rd party compilation process. However, this PR had to update the version of libbtc that is pulled down so that RIPEMD-160 is exposed.
    - The code makes certain assumptions regarding where public and private keys will be stored. This needs to be discussed and possibly altered later. For now, this isn't an issue, as no integration into actual BlockSettle code has occurred yet.
    - The logging has been commented out for now. It needs to be swapped out for something appropriate for BlockSettle.
    - libchacha20poly1305 points to a fork, much like libbtc. This fork includes CMake compatibility and removes a stray object file. An upstream PR will be filed.
    - Compilation is untested on Windows but received a visual sanity check against the other settings files.

--- build_scripts/libchacha20poly1305_settings.py
+++ build_scripts/libchacha20poly1305_settings.py
@@ -4,17 +4,17 @@ import shutil
 import subprocess
 
 from component_configurator import Configurator
-from build_scripts.mpir_settings import MPIRSettings
 
-
-class LibBTC(Configurator):
+# NB: This is a placeholder of sorts. It allows compilation on MacOS and *NIX
+# but there are no inherent files for building Windows. It may be necessary to
+# establish a fork, and maybe submit CMake files upstream.
+class LibChaCha20Poly1305Settings(Configurator):
     def __init__(self, settings):
         Configurator.__init__(self, settings)
-        self.mpir = MPIRSettings(settings)
-        self._version = '4caf71291ed1e7533c0162c4ed658d2aa6c72ea5'
-        self._package_name = 'libbtc'
+        self._version = '1132bf31c48e214adb296e3de1f96497bc15a1c0'
+        self._package_name = 'chacha20poly1305'
 
-        self._package_url = 'https://github.com/sergey-chernikov/' + self._package_name + '/archive/%s.zip' % self._version
+        self._package_url = 'https://github.com/droark/chacha20poly1305/archive/1132bf31c48e214adb296e3de1f96497bc15a1c0.zip'
 
     def get_package_name(self):
         return self._package_name + '-' + self._version
@@ -22,11 +22,11 @@ class LibBTC(Configurator):
     def get_revision_string(self):
         return self._version
 
-    def get_install_dir(self):
-        return os.path.join(self._project_settings.get_common_build_dir(), 'libbtc')
-
     def get_url(self):
         return self._package_url
+
+    def get_install_dir(self):
+        return os.path.join(self._project_settings.get_common_build_dir(), 'libchacha20poly1305')
 
     def is_archive(self):
         return True
@@ -34,7 +34,6 @@ class LibBTC(Configurator):
     def config(self):
         command = ['cmake',
                    self.get_unpacked_sources_dir(),
-                   '-DGMP_INSTALL_DIR=' + self.mpir.get_install_dir(),
                    '-G',
                    self._project_settings.get_cmake_generator()]
 
@@ -60,7 +59,7 @@ class LibBTC(Configurator):
         return result == 0
 
     def get_solution_file(self):
-        return 'libbtc.sln'
+        return 'libchacha20poly1305.sln'
 
     def get_win_build_configuration(self):
         if self._project_settings.get_build_mode() == 'release':
