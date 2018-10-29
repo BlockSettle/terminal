71d1c748bc1461186b97cbbc5042553d34e5ae89
Douglas Roark <joroark@vt.edu>
2018-10-28 22:28:28 -0700

    Complete initial compilation
    - Code didn't compile in the terminal. Make some changes to enable compilation.
    - Minor style change to better fit the BS coding style.

--- BlockSettleUILib/ExplorerWidget.cpp
+++ BlockSettleUILib/ExplorerWidget.cpp
@@ -1,36 +1,36 @@
-#include "explorerwidget.h"
-#include "ui_explorerwidget.h"
+#include "ExplorerWidget.h"
 #include <QStringListModel>
+#include "ui_ExplorerWidget.h"
+#include "UiUtils.h"
 
 ExplorerWidget::ExplorerWidget(QWidget *parent) :
-	TabWithShortcut(parent),
-    ui(new Ui::ExplorerWidget)
+   TabWithShortcut(parent)
+   , ui_(new Ui::ExplorerWidget())
 {
-    ui->setupUi(this);
+   ui_->setupUi(this);
 
-	connect(ui->searchBox, &QLineEdit::returnPressed, this, &ExplorerWidget::onSearchStarted);
-	
+   connect(ui_->searchBox, &QLineEdit::returnPressed, this, &ExplorerWidget::onSearchStarted);
 }
 
 ExplorerWidget::~ExplorerWidget() = default;
 
 void ExplorerWidget::shortcutActivated(ShortcutType s)
 {
-	switch (s) {
+   switch (s) {
 
-	default:
-		break;
-	}
+   default:
+      break;
+   }
 }
 
 void ExplorerWidget::onSearchStarted()
 {
-	ui->stackedWidget->count();
-	int index = ui->stackedWidget->currentIndex();
-	if (index < ui->stackedWidget->count() - 1)	{
-		ui->stackedWidget->setCurrentIndex(++index);
-	}
-	else {
-		ui->stackedWidget->setCurrentIndex(0);
-	}
+   ui_->stackedWidget->count();
+   int index = ui_->stackedWidget->currentIndex();
+   if (index < ui_->stackedWidget->count() - 1)	{
+      ui_->stackedWidget->setCurrentIndex(++index);
+   }
+   else {
+      ui_->stackedWidget->setCurrentIndex(0);
+   }
 }
--- BlockSettleUILib/ExplorerWidget.h
+++ BlockSettleUILib/ExplorerWidget.h
@@ -1,30 +1,31 @@
-#ifndef EXPLORERWIDGET_H
-#define EXPLORERWIDGET_H
+#ifndef __EXPLORERWIDGET_H__
+#define __EXPLORERWIDGET_H__
 
 #include <QWidget>
+#include <memory>
 #include "TabWithShortcut.h"
 
 namespace Ui {
-class ExplorerWidget;
+   class ExplorerWidget;
 }
 
-class QStringListModel;
+//class QStringListModel;
 
 class ExplorerWidget : public TabWithShortcut
 {
-    Q_OBJECT
+Q_OBJECT
 
 public:
     ExplorerWidget(QWidget *parent = nullptr);
     ~ExplorerWidget() override;
 
-	void shortcutActivated(ShortcutType s) override;
-
-private:
-	std::unique_ptr <Ui::ExplorerWidget> ui;
+   void shortcutActivated(ShortcutType s) override;
 
 protected slots:
-	void onSearchStarted();
+   void onSearchStarted();
+
+private:
+   std::unique_ptr<Ui::ExplorerWidget> ui_;
 };
 
 #endif // EXPLORERWIDGET_H
