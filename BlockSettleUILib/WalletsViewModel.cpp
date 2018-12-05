c3aff01448217133f092d1d43ca697a56607f90b
Sergey Chernikov <s.bilberry@gmail.com>
2018-12-05 23:22:06 +0300

    fixed myself

--- BlockSettleUILib/WalletsViewModel.cpp
+++ BlockSettleUILib/WalletsViewModel.cpp
@@ -582,8 +582,7 @@ void WalletsViewModel::LoadWallets(bool keepSelection)
          treeView->scrollTo(selection[0]);
       }
    }
-   // emit update address signal to WalletsWidget
-   updateAddresses();
+   emit updateAddresses();
 }
 
 void WalletsViewModel::onWalletChanged()
