c66df21fa0dfd98c05cfd6d869b2d78cf53b2ca9
Dmitry Herutskyi <dmitry.herutskyi@computools.com>
2019-02-11 17:10:03 +0200

    Added zbase32 decoding implementation; Refactored existing zbase encode implementation

--- BlocksettleNetworkingLib/ZBase32.cpp
+++ BlocksettleNetworkingLib/ZBase32.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <cmath>
+#include <algorithm>
 namespace {
    const std::vector<char> EncodingTable = std::vector<char>(
    {'y','b','n','d','r','f','g','8',
@@ -9,10 +10,7 @@ namespace {
     'o','t','1','u','w','i','s','z',
     'a','3','4','5','h','7','6','9'});
    
-   std::vector<unsigned char> DecodingTable = std::vector<unsigned char>(128, static_cast<unsigned char>(255));
-   
-   //TODO; fill this table and reuse, 
-   std::vector<unsigned char> DecodingTable1 = std::vector<unsigned char>({
+   const std::vector<unsigned char> DecodingTable = std::vector<unsigned char>({
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //0
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //8
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //16
@@ -20,50 +18,49 @@ namespace {
       
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //32
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //40
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //48
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //56
+            0xFF, 0x12, 0xFF, 0x19, 0x1A, 0x1B, 0x1E, 0x1D, //48
+            0x07, 0x1F, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //56
       
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //64
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //72
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //80
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //88
       
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //96
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //104
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //112
-            0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF  //120
+            0xFF, 0x18, 0x01, 0x0C, 0x03, 0x08, 0x05, 0x06, //96
+            0x1C, 0x15, 0x09, 0x0A, 0xFF, 0x0B, 0x02, 0x10, //104
+            0x0D, 0x0E, 0x04, 0x16, 0x11, 0x13, 0xFF, 0x14, //112
+            0x0F, 0x00, 0x17, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF  //120
    });
       
 }
 
 static bool ignoredSymbol(const char& symbol)
 {
-   return symbol >= DecodingTable.size() || DecodingTable[static_cast<size_t>(symbol)] == 255;
+   return symbol >= static_cast<int>(DecodingTable.size())
+         || DecodingTable[static_cast<size_t>(symbol)] == 0xFF;
 }
 
-static size_t createIndex(const std::string& data, size_t position, int* index)
+static size_t createIndex(const char* data, size_t data_length, size_t position, int* index_array)
 {
    int j = 0;
    while (j < 8) {
-      if (position >= data.length()){
-         index[j++] = -1;
+      if (position >= data_length){
+         index_array[j++] = -1;
          continue;
       }
       
-      if (ignoredSymbol(data.at(position))){
+      if (ignoredSymbol(data[position])){
          position++;
          continue;
       }
       
-      index[j] = data.at(position);
+      index_array[j] = data[position];
       j++;
       position++;
    }
    
    return position;
 }
-
-
 
 namespace bs { namespace utils {
    size_t zbase32_encode(char output[],
@@ -75,27 +72,23 @@ namespace bs { namespace utils {
       if (input == nullptr){
          return 0;
       }
-      
-      //std::vector<uint8_t> encodedResult;
-      size_t size = zbase32_encode_estimate_size(input_length);
-      //encodedResult.resize(size);
+
       size_t outIndex = 0;
-      for (int i = 0; i < input_length; i += 5) {
-         int byteCount = std::min(5, static_cast<int>(input_length - i));
+      for (size_t i = 0; i < input_length; i += 5) {
+         size_t byteCount = std::min(static_cast<size_t>(5), static_cast<size_t>(input_length - i));
          
-         size_t buffer = 0;
-         for (int j = 0; j < byteCount; ++j) {
+         uint64_t buffer = 0; // Should be >= 5 bytes, because max << (shift) for 5 bytes.
+         for (size_t j = 0; j < byteCount; ++j) {
             buffer = (buffer << 8) | input[i+j];
          }
          
-         int bitCount = byteCount * 8;
+         int bitCount = static_cast<int>(byteCount * 8);
          
          while(bitCount > 0){
             size_t index = bitCount >= 5
-                        ? (size_t)(buffer >> (bitCount - 5)) & 0x1f
-                        : (size_t)(buffer & (size_t)(0x1f >> (5 - bitCount))) << (5 - bitCount);
+                        ? static_cast<size_t>(buffer >> (bitCount - 5)) & 0x1f
+                        : (buffer & static_cast<uint64_t>(0x1f >> (5 - bitCount))) << (5 - bitCount);
             
-            //encodedResult.push_back(EncodingTable[index]);
             output[outIndex] = EncodingTable[index];
             bitCount -= 5;
             outIndex++;
@@ -107,43 +100,45 @@ namespace bs { namespace utils {
       return outIndex;
    }
    
-   size_t zbase32_encode_estimate_size(size_t data_size)
+   size_t zbase32_encode_estimate_size(size_t size)
    {
-      return static_cast<size_t>(ceilf(data_size * 8.0f / 5.0f));
+      return static_cast<size_t>(ceilf(size * 8.0f / 5.0f));
    }
-   
-   std::string decode(const std::string& data)
+
+   size_t zbase32_decode_estimate_size(size_t size)
    {
-      if (data.empty()){
-         return std::string();
+      return static_cast<size_t>(ceilf(size * 5.0f / 8.0f));
+   }
+
+   size_t zbase32_decode(uint8_t output[], const char input[], size_t input_length)
+   {
+      if (input_length <= 0) {
+         return 0;
       }
-      
-      for (size_t i= 0; i < EncodingTable.size(); ++i) {
-         DecodingTable.at(static_cast<size_t>(EncodingTable[i])) = static_cast<unsigned char>(i);
-      }
-      
-      std::vector<unsigned char> result = std::vector<unsigned char>(static_cast<size_t>(ceilf(data.length() * 5.0f / 8.0f)));
-      
-      int index[8] = {0};
-      for (size_t i = 0; i < data.length();) {
-         i = createIndex(data, i, index);
-         
+
+      size_t outputIndex = 0;
+      int index_array[8] = {0};
+      for (size_t pos = 0; pos < input_length;) {
+         pos = createIndex(input, input_length, pos, index_array);
+
          int shortByteCount = 0;
-         unsigned long long buffer = 0;
-         
-         for (int j = 0; j < 8 && index[j] != -1; ++j) {
-            buffer = (buffer << 5) | (unsigned long long)(DecodingTable[index[j]] & 0x1f);
+         uint64_t buffer = 0;
+
+         for (size_t j = 0; j < 8 && index_array[j] != -1; ++j) {
+            buffer = (buffer << 5) | static_cast<uint64_t>(DecodingTable[static_cast<size_t>(index_array[j])] & 0x1f);
             ++shortByteCount;
          }
-         
+
          int bitCount = shortByteCount * 5;
-         
+
          while (bitCount >= 8) {
-            unsigned char push = (unsigned char)((buffer >> (bitCount - 8)) & 0xff);
-            result.push_back(push);
+            uint8_t byte = static_cast<uint8_t>((buffer >> (bitCount - 8)) & 0xff);
+            output[outputIndex++] = byte;
             bitCount -= 8;
          }
       }
-      return std::string((char*)result.data(), result.size());
+      return outputIndex;
+
    }
+
 } }
--- BlocksettleNetworkingLib/ZBase32.h
+++ BlocksettleNetworkingLib/ZBase32.h
@@ -1,16 +1,19 @@
 #ifndef __BS_ZBASE32_H__
 #define __BS_ZBASE32_H__
 #include <inttypes.h>
-#include <unistd.h>
 #include <string>
+
 namespace bs {
    namespace utils {
       size_t zbase32_encode(char output[],
                             const uint8_t input[],
                             size_t input_length,
                             size_t& input_consumed);
-      size_t zbase32_encode_estimate_size(size_t data_size);
-      std::string decode(const std::string& data);
+      size_t zbase32_encode_estimate_size(size_t size);
+      size_t zbase32_decode(uint8_t output[],
+                            const char input[],
+                            size_t input_length);
+      size_t zbase32_decode_estimate_size(size_t size);
    }
 
 }
