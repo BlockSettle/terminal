e0b2699d1353b2aee7a6f606abfc4414f7aec17f
pokamest <pokamest@gmail.com>
2019-03-06 06:40:37 +0300

    manage armory servers fix - initial dialog

--- BlockSettleApp/main.cpp
+++ BlockSettleApp/main.cpp
@@ -15,7 +15,6 @@
 #include "BSTerminalSplashScreen.h"
 #include "BSTerminalMainWindow.h"
 #include "EncryptionUtils.h"
-#include "StartupDialog.h"
 #include "BSMessageBox.h"
 #include "ZMQHelperFunctions.h"
 
@@ -81,38 +80,6 @@ private:
    bool activationRequired_ = false;
 };
 
-static void checkFirstStart(ApplicationSettings *applicationSettings)
-{
-  bool wasInitialized = applicationSettings->get<bool>(ApplicationSettings::initialized);
-  if (wasInitialized) {
-    return;
-  }
-
-#ifdef _WIN32
-  // Read registry value in case it was set with installer. Could be used only on Windows for now.
-  QSettings settings(QLatin1String("HKEY_CURRENT_USER\\Software\\blocksettle\\blocksettle"), QSettings::NativeFormat);
-  bool showLicense = !settings.value(QLatin1String("license_accepted"), false).toBool();
-#else
-  bool showLicense = true;
-#endif // _WIN32
-
-  StartupDialog startupDialog(showLicense);
-  int result = startupDialog.exec();
-
-  if (result == QDialog::Rejected) {
-    std::exit(EXIT_FAILURE);
-  }
-
-  const bool runArmoryLocally = (startupDialog.runMode() == StartupDialog::RunMode::Local);
-  applicationSettings->set(ApplicationSettings::runArmoryLocally, runArmoryLocally);
-  applicationSettings->set(ApplicationSettings::netType, int(startupDialog.networkType()));
-
-  if (startupDialog.runMode() == StartupDialog::RunMode::Custom) {
-    applicationSettings->set(ApplicationSettings::armoryDbIp, startupDialog.armoryDbIp());
-    applicationSettings->set(ApplicationSettings::armoryDbPort, startupDialog.armoryDbPort());
-  }
-}
-
 static void checkStyleSheet(QApplication &app)
 {
    QLatin1String styleSheetFileName = QLatin1String("stylesheet.css");
@@ -135,7 +102,7 @@ static void checkStyleSheet(QApplication &app)
    app.setStyleSheet(QString::fromLatin1(stylesheetFile.readAll()));
 }
 
-static int GuiApp(int argc, char** argv)
+static int GuiApp(int &argc, char** argv)
 {
    Q_INIT_RESOURCE(armory);
 
@@ -221,8 +188,6 @@ static int GuiApp(int argc, char** argv)
       errorMessage.exec();
       return 1;
    }
-
-   checkFirstStart(settings.get());
 
    QString logoIcon;
    if (settings->get<NetworkType>(ApplicationSettings::netType) == NetworkType::MainNet) {
