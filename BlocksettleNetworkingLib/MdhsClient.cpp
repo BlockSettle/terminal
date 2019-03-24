a2cc97e3e50c48cd0ad8fb0d14b5d6becb42cf08
Ation <gation@gmail.com>
2019-03-24 23:02:47 +0200

    Remove unused const definitions. Replace tabs wih spaces

--- BlocksettleNetworkingLib/MdhsClient.cpp
+++ BlocksettleNetworkingLib/MdhsClient.cpp
@@ -9,84 +9,64 @@
 
 #include <spdlog/logger.h>
 
-// Private Market
-const std::string ANT_XBT = "ANT/XBT";
-const std::string BLK_XBT = "BLK/XBT";
-const std::string BSP_XBT = "BSP/XBT";
-const std::string JAN_XBT = "JAN/XBT";
-const std::string SCO_XBT = "SCO/XBT";
-// Spot XBT
-const std::string XBT_EUR = "XBT/EUR";
-const std::string XBT_GBP = "XBT/GBP";
-const std::string XBT_JPY = "XBT/JPY";
-const std::string XBT_SEK = "XBT/SEK";
-// Spot FX
-const std::string EUR_GBP = "EUR/GBP";
-const std::string EUR_JPY = "EUR/JPY";
-const std::string EUR_SEK = "EUR/SEK";
-const std::string GPB_JPY = "GPB/JPY";
-const std::string GBP_SEK = "GBP/SEK";
-const std::string JPY_SEK = "JPY/SEK";
-
-
 MdhsClient::MdhsClient(
-	const std::shared_ptr<ApplicationSettings>& appSettings,
-	const std::shared_ptr<ConnectionManager>& connectionManager,
-	const std::shared_ptr<spdlog::logger>& logger,
-	QObject* pParent)
-	: QObject(pParent)
-	, appSettings_(appSettings)
-	, connectionManager_(connectionManager)
-	, logger_(logger)
+   const std::shared_ptr<ApplicationSettings>& appSettings,
+   const std::shared_ptr<ConnectionManager>& connectionManager,
+   const std::shared_ptr<spdlog::logger>& logger,
+   QObject* pParent)
+   : QObject(pParent)
+   , appSettings_(appSettings)
+   , connectionManager_(connectionManager)
+   , logger_(logger)
 {
 }
 
 MdhsClient::~MdhsClient()
 {
-	FastLock locker(lockCommands_);
-	for (auto &cmd : activeCommands_) {
-		cmd->DropResult();
-	}
+   FastLock locker(lockCommands_);
+   for (auto &cmd : activeCommands_) {
+      cmd->DropResult();
+   }
 }
 
 void MdhsClient::SendRequest(const MarketDataHistoryRequest& request)
 {
-	const auto apiConnection = connectionManager_->CreateGenoaClientConnection();
-	auto command = std::make_shared<RequestReplyCommand>("MdhsClient", apiConnection, logger_);
+   const auto apiConnection = connectionManager_->CreateGenoaClientConnection();
+   auto command = std::make_shared<RequestReplyCommand>("MdhsClient", apiConnection, logger_);
 
-	command->SetReplyCallback([command, this](const std::string& data) -> bool
-	{
-		command->CleanupCallbacks();
-		FastLock locker(lockCommands_);
-		activeCommands_.erase(command);
-		return OnDataReceived(data);
-	});
+   command->SetReplyCallback([command, this](const std::string& data) -> bool
+   {
+      command->CleanupCallbacks();
+      FastLock locker(lockCommands_);
+      activeCommands_.erase(command);
+      return OnDataReceived(data);
+   });
 
-	command->SetErrorCallback([command, this](const std::string& message)
-	{
-		logger_->error("Failed to get history data from mdhs: {}", message);
-		command->CleanupCallbacks();
-		FastLock locker(lockCommands_);
-		activeCommands_.erase(command);
-	});
+   command->SetErrorCallback([command, this](const std::string& message)
+   {
+      logger_->error("Failed to get history data from mdhs: {}", message);
+      command->CleanupCallbacks();
+      FastLock locker(lockCommands_);
+      activeCommands_.erase(command);
+   });
 
-	FastLock locker(lockCommands_);
-	activeCommands_.emplace(command);
+   FastLock locker(lockCommands_);
+   activeCommands_.emplace(command);
 
-	if (!command->ExecuteRequest(
-		appSettings_->get<std::string>(ApplicationSettings::mdhsHost),
-		appSettings_->get<std::string>(ApplicationSettings::mdhsPort),
-		request.SerializeAsString()))
-	{
-		logger_->error("Failed to send request for mdhs.");
-		command->CleanupCallbacks();
-		FastLock locker(lockCommands_);
-		activeCommands_.erase(command);
-	}
+   if (!command->ExecuteRequest(
+      appSettings_->get<std::string>(ApplicationSettings::mdhsHost),
+      appSettings_->get<std::string>(ApplicationSettings::mdhsPort),
+      request.SerializeAsString()))
+   {
+      logger_->error("Failed to send request for mdhs.");
+      command->CleanupCallbacks();
+      FastLock locker(lockCommands_);
+      activeCommands_.erase(command);
+   }
 }
 
 bool MdhsClient::OnDataReceived(const std::string& data)
 {
-	emit DataReceived(data);
-	return true;
+   emit DataReceived(data);
+   return true;
 }
