39192d8325cd54f3d9c256f2937b36de37eb0b09
Ation <gation@gmail.com>
2019-04-19 11:17:07 +0300

    Use specific revision

--- build_scripts/thread_pool_settings.py
+++ build_scripts/thread_pool_settings.py
@@ -10,17 +10,16 @@ from component_configurator import Configurator
 class ThreadPoolSettings(Configurator):
     def __init__(self, settings):
         Configurator.__init__(self, settings)
-        self._branch = 'master'
         self._package_name = 'thread-pool-cpp'
         self._script_revision = '1'
-
-        self._package_url = 'https://github.com/inkooboo/thread-pool-cpp/archive/' + self._branch + '.zip'
+        self._git_revision = 'af95dd88daa094f67bbd178b639c7282373a3b09'
+        self._package_url = 'https://github.com/inkooboo/thread-pool-cpp/archive/' + self._git_revision + '.zip'
 
     def get_package_name(self):
-        return self._package_name + '-' + self._branch
+        return self._package_name + '-' + self._git_revision
 
     def get_revision_string(self):
-        return self._script_revision
+        return self._git_revision + '_' + self._script_revision
 
     def get_url(self):
         return self._package_url
