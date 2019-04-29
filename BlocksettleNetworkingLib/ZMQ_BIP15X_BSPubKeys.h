749e54bc2ef7fd84b07ffb48e6542801ad10fdd5
Douglas Roark <joroark@vt.edu>
2019-04-28 18:31:05 -0700

    Add const to pub key string

--- BlocksettleNetworkingLib/ZMQ_BIP15X_BSPubKeys.h
+++ BlocksettleNetworkingLib/ZMQ_BIP15X_BSPubKeys.h
@@ -5,7 +5,7 @@
 // BlockSettle. This file is used to hard-code acceptable keys for particular
 // connections.
 
-static std::string publicBridgeKey =
+const static std::string publicBridgeKey =
    "033f116fc81ec3cfb0d29154d76321dd86dbec5ea9c0c6a3d8db9d986522c75927";
 
 #endif // __ZMQ_BIP15X_SERVERCONNECTION_H__
