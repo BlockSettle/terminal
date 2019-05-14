adace46f967ba21917972e9d2ab4661c28228d54
Sergey Chernikov <s.bilberry@gmail.com>
2019-05-14 19:19:23 +0300

    Fixed typo in 'Canceled'

--- BlocksettleNetworkingLib/ArmoryServersProvider.cpp
+++ BlocksettleNetworkingLib/ArmoryServersProvider.cpp
@@ -28,7 +28,6 @@ QList<ArmoryServer> ArmoryServersProvider::servers() const
 
    // #1 add MainNet blocksettle server
    ArmoryServer bsMainNet = defaultServers_.at(0);
-   //bsMainNet.armoryDBPort = appSettings_->GetDefaultArmoryRemotePort(NetworkType::MainNet);
    if (defaultServersKeys.size() >= 1) {
       bsMainNet.armoryDBKey = defaultServersKeys.at(0);
    }
@@ -36,7 +35,6 @@ QList<ArmoryServer> ArmoryServersProvider::servers() const
 
    // #2 add TestNet blocksettle server
    ArmoryServer bsTestNet = defaultServers_.at(1);
-   //bsTestNet.armoryDBPort = appSettings_->GetDefaultArmoryRemotePort(NetworkType::TestNet);
    if (defaultServersKeys.size() >= 2) {
       bsTestNet.armoryDBKey = defaultServersKeys.at(1);
    }
@@ -308,5 +306,3 @@ void ArmoryServersProvider::setConnectedArmorySettings(const ArmorySettings &cur
 {
    connectedArmorySettings_ = currentArmorySettings;
 }
-
-
