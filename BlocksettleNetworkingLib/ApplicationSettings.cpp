ebacf693799fe18d0757630d04993381d3432cf4
Sergey Chernikov <s.bilberry@gmail.com>
2019-05-14 19:18:38 +0300

    BST-1868: add hard-coded Armory clusters keys

--- BlocksettleNetworkingLib/ApplicationSettings.cpp
+++ BlocksettleNetworkingLib/ApplicationSettings.cpp
@@ -158,7 +158,9 @@ ApplicationSettings::ApplicationSettings(const QString &appName
       { remoteSigners,                    SettingDef(QLatin1String("RemoteSignerKeys")) },
       { rememberLoginUserName,            SettingDef(QLatin1String("RememberLoginUserName"), true) },
       { armoryServers,                    SettingDef(QLatin1String("ArmoryServers")) },
-      { defaultArmoryServersKeys,         SettingDef(QLatin1String("DefaultArmoryServersKeys")) },
+      { defaultArmoryServersKeys,         SettingDef(QLatin1String("DefaultArmoryServersKeys"), QStringList()
+         << QLatin1String("0350ac2d232e0b0fb5c68a821ff8e50e42608aa62c5755485e6caa8bc14e5918ea")       // mainnet Armory cluster key
+         << QLatin1String("02ed6116a7844cae8a1dc4d5fb27922594b79cc41df081d84d2f36983757904de5")) },   // testnet Armory cluster key
       { startupBIP150CTX,                 SettingDef(QLatin1String("StartupBIP150CTX"), false) }
    };
 }
