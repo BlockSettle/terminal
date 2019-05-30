2860795ae44cd51362713e98b81eea1b4c631141
Jack L <zheka_93@ukr.net>
2019-05-30 15:19:10 +0300

    OTC Chat: fixed messages read status

--- BlockSettleUILib/ChatWidget.h
+++ BlockSettleUILib/ChatWidget.h
@@ -64,6 +64,7 @@ public:
    bool hasUnreadMessages();
    void switchToChat(const QString& chatId);
    void setCelerClient(std::shared_ptr<CelerClient> celerClient);
+   void updateChat(const bool &isChatTab);
 
 public slots:
    void onLoggedOut();
@@ -147,6 +148,7 @@ private:
    QSpacerItem *chatUsersVerticalSpacer_;
    QTimer *popupVisibleTimer_;
    bool isChatMessagesSelected_;
+   bool isChatTab_;
 
 private:
    std::shared_ptr<ChatWidgetState> stateCurrent_;
