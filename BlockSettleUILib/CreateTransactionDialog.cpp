4ef850c2a87549012a4e10871168c606ec5ed754
Evgeni Biryukov <evgeni.biryuk@gmail.com>
2019-05-31 14:17:05 +0300

    Updated text for confirmation on abort create transaction

--- BlockSettleUILib/CreateTransactionDialog.cpp
+++ BlockSettleUILib/CreateTransactionDialog.cpp
@@ -144,9 +144,9 @@ void CreateTransactionDialog::clear()
 void CreateTransactionDialog::reject()
 {
    if (broadcasting_) {
-      BSMessageBox confirmExit(BSMessageBox::question, tr("Abort transaction broadcasting")
-         , tr("You are about to abort transaction sending")
-         , tr("Are you sure you wish to abort the signing and sending process?"), this);
+      BSMessageBox confirmExit(BSMessageBox::question, tr("Abort transaction broadcast")
+         , tr("Abort transaction broadcast?")
+         , tr("Are you sure you wish to abort the signing and broadcast process?"), this);
       //confirmExit.setExclamationIcon();
       if (confirmExit.exec() != QDialog::Accepted) {
          return;
