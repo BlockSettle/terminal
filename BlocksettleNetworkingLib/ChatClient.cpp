ad74921041de23872518cf39bd3f5c26703b9873
Evgeni Biryukov <evgeni.biryuk@gmail.com>
2019-06-21 17:42:31 +0300

    Fixed read contacts from database

--- BlocksettleNetworkingLib/ChatClient.cpp
+++ BlocksettleNetworkingLib/ChatClient.cpp
@@ -96,6 +96,7 @@ void ChatClient::readDatabase()
       auto contact = std::make_shared<Chat::Data>();
       auto d = contact->mutable_contact_record();
       d->set_user_id(model_->currentUser());
+      d->set_contact_id(c.contact_id());
       d->set_status(c.status());
       d->set_display_name(c.display_name());
       model_->insertContactObject(contact);
