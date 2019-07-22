80a61da49ea6de2765b2cacaf471b644dbb29237
Pavel Kokolemin <pavelkok@gmail.com>
2019-07-22 13:41:51 +0300

    Make Linux build script a bit more robust

--- Deploy/deploy.sh
+++ Deploy/deploy.sh
@@ -19,18 +19,17 @@ if [ ! -d $libprotobuf ]; then
    exit
 fi
 
+mkdir -p Ubuntu/usr/bin
+mkdir -p Ubuntu/lib/x86_64-linux-gnu
 rm -f Ubuntu/usr/bin/RFQBot.qml
 rm -rf Ubuntu/usr/share/blocksettle/scripts
 mkdir -p Ubuntu/usr/share/blocksettle/scripts
 
-mkdir -p Ubuntu/usr/bin
-mkdir -p Ubuntu/lib/x86_64-linux-gnu
-
-cp -f $binpath/* Ubuntu/usr/bin/
-cp -f $scriptpath/DealerAutoQuote.qml Ubuntu/usr/share/blocksettle/scripts/
-cp -f $scriptpath/RFQBot.qml Ubuntu/usr/share/blocksettle/scripts/
-cp -fP $libprotobuf/libprotobuf.so* Ubuntu/lib/x86_64-linux-gnu
-cp -f $libprotobuf/libprotobuf.la Ubuntu/lib/x86_64-linux-gnu
+cp $binpath/* Ubuntu/usr/bin/
+cp $scriptpath/DealerAutoQuote.qml Ubuntu/usr/share/blocksettle/scripts/
+cp $scriptpath/RFQBot.qml Ubuntu/usr/share/blocksettle/scripts/
+cp -P $libprotobuf/libprotobuf.so* Ubuntu/lib/x86_64-linux-gnu/
+cp $libprotobuf/libprotobuf.la Ubuntu/lib/x86_64-linux-gnu/
 
 dpkg -b Ubuntu bsterminal.deb
 echo "deb package generated"
