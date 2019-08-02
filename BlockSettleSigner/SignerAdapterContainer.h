a3999aed2f7e51abe64650c2d493d1bca22b331a
Sergey Chernikov <s.bilberry@gmail.com>
2019-08-02 15:43:59 +0300

    BST-2100: fix CC leaf creation

--- BlockSettleSigner/SignerAdapterContainer.h
+++ BlockSettleSigner/SignerAdapterContainer.h
@@ -64,8 +64,7 @@ public:
 
    bool createHDLeaf(const std::string &rootWalletId, const bs::hd::Path &
       , const std::vector<bs::wallet::PasswordData> &pwdData = {}
-      , bs::sync::PasswordDialogData = {}
-      , const std::function<void(bs::error::ErrorCode result)> &cb = nullptr) override { return false; }
+      , bs::sync::PasswordDialogData = {}, const CreateHDLeafCb &cb = nullptr) override { return false; }
 
    bs::signer::RequestId DeleteHDRoot(const std::string &rootWalletId) override;
    bs::signer::RequestId DeleteHDLeaf(const std::string &) override { return 0; }
