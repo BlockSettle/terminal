ae4b0d2dffd526769140be6e0cc120654b436f25
Pavel Kokolemin <pavelkok@gmail.com>
2019-08-17 10:58:20 +0300

    Hide settlement wallet from generate address dialog

--- BlockSettleUILib/WalletsViewModel.cpp
+++ BlockSettleUILib/WalletsViewModel.cpp
@@ -237,7 +237,7 @@ public:
 
    void addLeaves(const std::vector<std::shared_ptr<bs::sync::Wallet>> &leaves) {
       for (const auto &leaf : leaves) {
-         if (viewModel_->showRegularWallets() && (leaf == viewModel_->getAuthWallet())) {
+         if (viewModel_->showRegularWallets() && (leaf->type() != bs::core::wallet::Type::Bitcoin)) {
             continue;
          }
          const auto leafNode = new WalletLeafNode(viewModel_, leaf, nbChildren(), this);
@@ -251,11 +251,8 @@ public:
 void WalletRootNode::addGroups(const std::vector<std::shared_ptr<bs::sync::hd::Group>> &groups)
 {
    for (const auto &group : groups) {
-      if (viewModel_->showRegularWallets()) {
-         if ((group->type() == bs::core::wallet::Type::Authentication)
-            || (group->type() == bs::core::wallet::Type::ColorCoin)) {
-            continue;
-         }
+      if (viewModel_->showRegularWallets() && (group->type() != bs::core::wallet::Type::Bitcoin)) {
+         continue;
       }
       const auto groupNode = new WalletGroupNode(viewModel_, group->name(), group->description()
          , getNodeType(group->type()), nbChildren(), this);
