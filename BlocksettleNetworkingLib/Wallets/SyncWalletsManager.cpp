b6561c09f57a6eae36704a8c1d3fcaac7d615665
Sergey Chernikov <s.bilberry@gmail.com>
2019-08-22 15:26:02 +0300

    BST-2160: update sync wallets after promotion

--- BlocksettleNetworkingLib/Wallets/SyncWalletsManager.cpp
+++ BlocksettleNetworkingLib/Wallets/SyncWalletsManager.cpp
@@ -949,6 +949,47 @@ void WalletsManager::onWalletsListUpdated()
             });
             newWallets_.insert(hdWallet.first);
          }
+         else {
+            const auto wallet = hdWallets_[hdWallet.first];
+            const auto &cbSyncHD = [this, wallet](bs::sync::HDWalletData hdData) {
+               bool walletUpdated = false;
+               if (hdData.groups.size() != wallet->getGroups().size()) {
+                  walletUpdated = true;
+               }
+               else {
+                  for (const auto &group : hdData.groups) {
+                     const auto hdGroup = wallet->getGroup(group.type);
+                     if (hdGroup->getLeaves().size() != group.leaves.size()) {
+                        walletUpdated = true;
+                        break;
+                     }
+                  }
+               }
+               if (!walletUpdated) {
+                  return;
+               }
+               logger_->debug("[WalletsManager::onWalletsListUpdated] wallet {} has changed - resyncing"
+                  , wallet->walletId());
+               wallet->synchronize([this, wallet] {
+                  wallet->registerWallet(armoryPtr_);
+                  for (const auto &leaf : wallet->getLeaves()) {
+                     if (!getWalletById(leaf->walletId())) {
+                        logger_->debug("[WalletsManager::onWalletsListUpdated] adding new leaf {}"
+                           , leaf->walletId());
+                        addWallet(leaf, true);
+                     }
+                  }
+                  wallet->scan([this, wallet](bs::sync::SyncState state) {
+                     if (state == bs::sync::SyncState::Success) {
+                        QMetaObject::invokeMethod(this, [this, wallet] {
+                           emit walletChanged(wallet->walletId());
+                        });
+                     }
+                  });
+               });
+            };
+            signContainer_->syncHDWallet(wallet->walletId(), cbSyncHD);
+         }
       }
       const auto hdWalledsId = hdWalletsId_;
       for (const auto &hdWalletId : hdWalledsId) {
