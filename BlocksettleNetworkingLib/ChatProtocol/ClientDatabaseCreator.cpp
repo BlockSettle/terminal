4aa9ba7d9cbe5ab9e724b61f795263f216a025eb
thormak <thormak81@gmail.com>
2019-09-05 04:19:55 +0200

    Change constants variable format

--- BlocksettleNetworkingLib/ChatProtocol/ClientDatabaseCreator.cpp
+++ BlocksettleNetworkingLib/ChatProtocol/ClientDatabaseCreator.cpp
@@ -5,51 +5,51 @@ namespace Chat
 
    namespace {
       namespace party {
-         const QString TABLE_NAME = QLatin1String("party");
-         const QString PARTY_TABLE_ID = QLatin1String("id");
-         const QString PARTY_ID = QLatin1String("party_id");
-         const QString PARTY_DISPLAY_NAME = QLatin1String("party_display_name");
+         const QString kTableName = QLatin1String("party");
+         const QString kPartyTableId = QLatin1String("id");
+         const QString kPartyId = QLatin1String("party_id");
+         const QString kPartyDisplayName = QLatin1String("party_display_name");
       }
 
       namespace partyMessage {
-         const QString TABLE_NAME = QLatin1String("party_message");
-         const QString PARTY_MESSAGE_TABLE_ID = QLatin1String("id");
-         const QString PARTY_TABLE_ID = QLatin1String("party_table_id");
-         const QString MESSAGE_ID = QLatin1String("message_id");
-         const QString TIMESTAMP = QLatin1String("timestamp");
-         const QString MESSAGE_STATE = QLatin1String("message_state");
-         const QString ENCRYPTION_TYPE = QLatin1String("encryption_type");
-         const QString NONCE = QLatin1String("nonce");
-         const QString MESSAGE_TEXT = QLatin1String("message_text");
-         const QString SENDER = QLatin1String("sender");
+         const QString kTableName = QLatin1String("party_message");
+         const QString kPartyMessageTableId = QLatin1String("id");
+         const QString kPartyTableId = QLatin1String("party_table_id");
+         const QString kMessageId = QLatin1String("message_id");
+         const QString kTimestamp = QLatin1String("timestamp");
+         const QString kMessageState = QLatin1String("message_state");
+         const QString kEncryptionType = QLatin1String("encryption_type");
+         const QString kNonce = QLatin1String("nonce");
+         const QString kMessageText = QLatin1String("message_text");
+         const QString kSender = QLatin1String("sender");
       }
    }
 
    const static QMap <QString, TableStructure> clientTablesMap {
-      {party::TABLE_NAME,
+      {party::kTableName,
          {
             { //Table columns
-               {party::PARTY_TABLE_ID, QLatin1String("INTEGER PRIMARY KEY AUTOINCREMENT")},
-               {party::PARTY_ID, QLatin1String("TEXT NOT NULL")},
-               {party::PARTY_DISPLAY_NAME, QLatin1String("TEXT NOT NULL")}
+               {party::kPartyTableId, QLatin1String("INTEGER PRIMARY KEY AUTOINCREMENT")},
+               {party::kPartyId, QLatin1String("TEXT NOT NULL")},
+               {party::kPartyDisplayName, QLatin1String("TEXT NOT NULL")}
             }
          }
       },
-      {partyMessage::TABLE_NAME,
+      {partyMessage::kTableName,
          {
             { //Table columns
-               {partyMessage::PARTY_MESSAGE_TABLE_ID, QLatin1String("INTEGER PRIMARY KEY AUTOINCREMENT")},
-               {partyMessage::PARTY_TABLE_ID, QLatin1String("INTEGER NOT NULL")},
-               {partyMessage::MESSAGE_ID, QLatin1String("CHAR(32) NOT NULL")},
-               {partyMessage::TIMESTAMP, QLatin1String("INTEGER NOT NULL")},
-               {partyMessage::MESSAGE_STATE, QLatin1String("INTEGER NOT NULL")},
-               {partyMessage::ENCRYPTION_TYPE, QLatin1String("INTEGER NOT NULL")},
-               {partyMessage::NONCE, QLatin1String("BLOB")},
-               {partyMessage::MESSAGE_TEXT, QLatin1String("TEXT")},
-               {partyMessage::SENDER, QLatin1String("TEXT")}
+               {partyMessage::kPartyMessageTableId, QLatin1String("INTEGER PRIMARY KEY AUTOINCREMENT")},
+               {partyMessage::kPartyTableId, QLatin1String("INTEGER NOT NULL")},
+               {partyMessage::kMessageId, QLatin1String("CHAR(32) NOT NULL")},
+               {partyMessage::kTimestamp, QLatin1String("INTEGER NOT NULL")},
+               {partyMessage::kMessageState, QLatin1String("INTEGER NOT NULL")},
+               {partyMessage::kEncryptionType, QLatin1String("INTEGER NOT NULL")},
+               {partyMessage::kNonce, QLatin1String("BLOB")},
+               {partyMessage::kMessageText, QLatin1String("TEXT")},
+               {partyMessage::kSender, QLatin1String("TEXT")}
             },
             { //Foreign keys
-               {partyMessage::PARTY_TABLE_ID, party::TABLE_NAME, party::PARTY_TABLE_ID}
+               {partyMessage::kPartyTableId, party::kTableName, party::kPartyTableId}
             }
          }
       }
@@ -65,8 +65,8 @@ namespace Chat
    {
       requiredTables_ = QStringList(
          {
-            party::TABLE_NAME,
-            partyMessage::TABLE_NAME
+            party::kTableName,
+            partyMessage::kTableName
          }
       );
 
