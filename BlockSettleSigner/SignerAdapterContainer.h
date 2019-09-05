38d3183156e27254044b9bff3fd9d7418cc9bcbe
Sergey Chernikov <s.bilberry@gmail.com>
2019-09-05 18:04:52 +0300

    BST-2207: fix for interface change

--- BlockSettleSigner/SignerAdapterContainer.h
+++ BlockSettleSigner/SignerAdapterContainer.h
@@ -58,6 +58,9 @@ public:
 
    bs::signer::RequestId signMultiTXRequest(const bs::core::wallet::TXMultiSignRequest &) override { return 0; }
 
+   bs::signer::RequestId signAuthRevocation(const std::string &walletId, const bs::Address &authAddr
+      , const UTXO &, const bs::Address &bsAddr, const SignTxCb &cb = nullptr) override { return 0; }
+
    bs::signer::RequestId updateDialogData(const bs::sync::PasswordDialogData &, uint32_t = 0) override { return 0; }
    bs::signer::RequestId CancelSignTx(const BinaryData &tx) override { return 0; }
 
