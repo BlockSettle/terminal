7c254cb0015b06613218b6d84bd1e3714df1ded2
Pavel Kokolemin <pavelkok@gmail.com>
2019-10-05 15:02:47 +0300

    Update proto file

--- BlockSettleUILib/OrderListModel.cpp
+++ BlockSettleUILib/OrderListModel.cpp
@@ -602,6 +602,7 @@ void OrderListModel::processUpdateOrders(const Blocksettle::Communication::Proxy
       order.dateTime = QDateTime::fromMSecsSinceEpoch(data.timestamp_ms());
       order.product = data.product();
       order.quantity = data.quantity();
+      order.security = data.security();
       order.price = data.price();
 
       onOrderUpdated(order);
