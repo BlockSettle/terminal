4af6793c8a46b920399e007ef4834dadd0d7992f
Ation <gation@gmail.com>
2019-10-06 00:47:13 +0300

    Add cached fee estimations

---
+++ BlocksettleNetworkingLib/BitcoinFeeCache.cpp
@@ -0,0 +1,98 @@
+#include "BitcoinFeeCache.h"
+
+#include "ArmoryConnection.h"
+
+static constexpr auto kCacheValueExpireTimeout = std::chrono::hours(1);
+
+BitcoinFeeCache::BitcoinFeeCache(const std::shared_ptr<spdlog::logger> &logger
+   , const std::shared_ptr<ArmoryConnection> &armory)
+ : logger_{logger}
+ , armory_{armory}
+{
+}
+
+//fee = ArmoryConnection::toFeePerByte(fee);
+bool BitcoinFeeCache::getFeePerByteEstimation(unsigned int blocksToWait, const feeCB& cb)
+{
+   auto blocks = blocksToWait;
+   if (blocksToWait < 2) {
+      blocksToWait = 2;
+   } else if (blocksToWait > 144) {
+      blocksToWait = 144;
+   }
+
+   bool  estimateScheduled = false;
+   float result = 0;
+
+   {
+      std::lock_guard<std::mutex> lock(cacheMutex_);
+
+      auto it = estimationsCache_.find(blocksToWait);
+      if ((it == estimationsCache_.end()) || cacheValueExpired(it->second.estimationTimestamp)) {
+         auto it = pendingCB_.find(blocksToWait);
+
+         estimateScheduled = true;
+
+         if (it == pendingCB_.end()) {
+            pendingCB_.emplace(blocksToWait, std::vector<feeCB>{ cb });
+
+            auto cbWrap = [this, blocksToWait](float fee) {
+               setFeeEstimationValue(blocksToWait, ArmoryConnection::toFeePerByte(fee));
+            };
+
+            if (!armory_->estimateFee(blocksToWait, cbWrap)) {
+               pendingCB_.erase(blocksToWait);
+               return false;
+            }
+
+         } else {
+            it->second.emplace_back(cb);
+         }
+      } else {
+         estimateScheduled = false;
+         result = it->second.feeEstimation;
+      }
+   }
+
+   if (!estimateScheduled) {
+      cb(result);
+   }
+
+   return true;
+}
+
+void BitcoinFeeCache::setFeeEstimationValue(const unsigned int blocksToWait, float fee)
+{
+   std::vector<feeCB> userCB;
+
+   {
+      std::lock_guard<std::mutex> lock(cacheMutex_);
+
+      FeeEstimationCache currentValue;
+      currentValue.feeEstimation = fee;
+      currentValue.estimationTimestamp = std::chrono::system_clock::now();
+
+      estimationsCache_[blocksToWait] = currentValue;
+
+      auto cbIt = pendingCB_.find(blocksToWait);
+      if (cbIt != pendingCB_.end()) {
+         userCB = std::move(cbIt->second);
+
+         pendingCB_.erase(cbIt);
+      }
+   }
+
+   if (userCB.empty()) {
+      logger_->error("[BitcoinFeeCache::setFeeEstimationValue] no CB saved for {} blocks"
+         , blocksToWait);
+   }
+
+   for (const auto& cb : userCB) {
+      cb(fee);
+   }
+}
+
+bool BitcoinFeeCache::cacheValueExpired(const std::chrono::system_clock::time_point& timestamp) const
+{
+   return (std::chrono::system_clock::now() - timestamp) > kCacheValueExpireTimeout;
+}
---
+++ BlocksettleNetworkingLib/BitcoinFeeCache.h
@@ -0,0 +1,53 @@
+#ifndef __BITCOIN_FEE_CACHE_H__
+#define __BITCOIN_FEE_CACHE_H__
+
+#include <spdlog/spdlog.h>
+
+#include <memory>
+#include <mutex>
+#include <chrono>
+#include <unordered_map>
+#include <vector>
+
+class ArmoryConnection;
+
+class BitcoinFeeCache
+{
+public:
+   BitcoinFeeCache(const std::shared_ptr<spdlog::logger> &logger
+                   , const std::shared_ptr<ArmoryConnection> &armory);
+   ~BitcoinFeeCache() noexcept = default;
+
+   BitcoinFeeCache(const BitcoinFeeCache&) = delete;
+   BitcoinFeeCache& operator = (const BitcoinFeeCache&) = delete;
+
+   BitcoinFeeCache(BitcoinFeeCache&&) = delete;
+   BitcoinFeeCache& operator = (BitcoinFeeCache&&) = delete;
+
+   // return s/b, not armory original bitcoin/kb
+   using feeCB = std::function<void(float)>;
+
+   bool getFeePerByteEstimation(unsigned int blocksToWait, const feeCB& cb);
+
+private:
+   bool cacheValueExpired(const std::chrono::system_clock::time_point& timestamp) const;
+
+   void setFeeEstimationValue(const unsigned int blocksToWait, float fee);
+
+private:
+   std::shared_ptr<spdlog::logger>     logger_;
+   std::shared_ptr<ArmoryConnection>   armory_;
+
+
+   struct FeeEstimationCache
+   {
+      float                                  feeEstimation;
+      std::chrono::system_clock::time_point  estimationTimestamp;
+   };
+
+   std::mutex                                            cacheMutex_;
+   std::unordered_map<unsigned int, FeeEstimationCache>  estimationsCache_;
+   std::unordered_map<unsigned int, std::vector<feeCB>>  pendingCB_;
+};
+
+#endif
