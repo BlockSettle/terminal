24c907f8ed5b0b12ed12d05fe23f2afc6de9139c
Pavel Kokolemin <pavelkok@gmail.com>
2019-10-10 16:33:47 +0300

    Update blotter styles a bit
    Use orange color for new orders

--- BlockSettleUILib/OrderListModel.cpp
+++ BlockSettleUILib/OrderListModel.cpp
@@ -343,36 +343,37 @@ void OrderListModel::setOrderStatus(Group *group, int index, const bs::network::
    switch (order.status)
    {
       case bs::network::Order::New:
-         group->rows_[static_cast<std::size_t>(index)]->status_ = tr("New");
-         break;
-      case bs::network::Order::Pending:
-         group->rows_[static_cast<std::size_t>(index)]->status_ = tr("Pending");
-         if (!order.pendingStatus.empty()) {
-            auto statusString = QString::fromStdString(order.pendingStatus);
-            group->rows_[static_cast<std::size_t>(index)]->status_ = statusString;
-            if (statusString.startsWith(QLatin1String("Revoke"))) {
-               group->rows_[static_cast<std::size_t>(index)]->statusColor_ =
-                  QColor{0xf6, 0xa7, 0x24};
-               break;
-            }
+         // New is not used currently
+      case bs::network::Order::Pending: {
+         const auto status = order.pendingStatus.empty() ? tr("Pending") : QString::fromStdString(order.pendingStatus);
+
+         auto color = QColor{0x63, 0xB0, 0xB2};
+         if (status.toLower().startsWith(QStringLiteral("revoke"))) {
+             color = QColor{0xf6, 0xa7, 0x24};
+         } else if (status.toLower().startsWith(QStringLiteral("pay-in")) || order.pendingStatus.empty()) {
+            // try to use different color for new orders
+            color = QColor{0xff, 0x7f, 0};
          }
-         group->rows_[static_cast<std::size_t>(index)]->statusColor_ = QColor{0x63, 0xB0, 0xB2};
+
+         group->rows_[size_t(index)]->status_ = status;
+         group->rows_[size_t(index)]->statusColor_ = color;
          break;
+      }
 
       case bs::network::Order::Filled:
-         group->rows_[static_cast<std::size_t>(index)]->status_ = tr("Settled");
-         group->rows_[static_cast<std::size_t>(index)]->statusColor_ = QColor{0x22, 0xC0, 0x64};
+         group->rows_[size_t(index)]->status_ = tr("Settled");
+         group->rows_[size_t(index)]->statusColor_ = QColor{0x22, 0xC0, 0x64};
          break;
 
       case bs::network::Order::Failed:
-         group->rows_[static_cast<std::size_t>(index)]->status_ = tr("Failed");
-         group->rows_[static_cast<std::size_t>(index)]->statusColor_ = QColor{0xEC, 0x0A, 0x35};
+         group->rows_[size_t(index)]->status_ = tr("Failed");
+         group->rows_[size_t(index)]->statusColor_ = QColor{0xEC, 0x0A, 0x35};
          break;
    }
 
    if (emitUpdate) {
       const auto idx = createIndex(index, Header::Status,
-         &group->rows_[static_cast<std::size_t>(index)]->idx_);
+         &group->rows_[size_t(index)]->idx_);
 
       emit dataChanged(idx, idx);
    }
