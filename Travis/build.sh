d9ea797f9ca41bbaaa6b3c64c15a418739b87485
pokamest <pokamest@gmail.com>
2019-10-19 21:22:48 +0300

    Travis fix

--- Travis/build.sh
+++ Travis/build.sh
@@ -3,6 +3,16 @@ echo "Build script started ..."
 
 set -o errexit -o nounset
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=MacOS;;
+    CYGWIN*)    machine=Windows;;
+    MINGW*)     machine=Windows;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+
 # Hold on to current directory
 project_dir=$(pwd)
 script_dir=${project_dir}/Travis/
@@ -13,7 +23,7 @@ echo "Project dir: ${project_dir}"
 echo "3rd Party dir: ${third_dir}"
 
 TRAVIS_TAG=${TRAVIS_TAG:-$(date +'%Y.%m.%d_%H.%M.%S')-$(git log --format=%h -1)}
-APP_FILE_NAME=BlockSettle_${TRAVIS_TAG}_MacOS
+APP_FILE_NAME=BlockSettle_${TRAVIS_TAG}_${machine}
 
 echo "Building for branch ${TRAVIS_BRANCH}"
 echo "Building TAG ${TRAVIS_TAG}"
@@ -21,6 +31,7 @@ echo "Building TAG ${TRAVIS_TAG}"
 ZIP_FILE_NAME=${APP_FILE_NAME}.tar.gz
 
 echo "Target file is ${ZIP_FILE_NAME}"
+echo "Building on platform: $(lsb_release -a)"
 
 # Build App
 # Due 120 minutes limit build either 3rd party as first step or project as second step
@@ -34,6 +45,7 @@ then
    exit 0
 else
    echo "Building App..."
+   python3 generate.py
    cd ${project_dir}/terminal.release
    make -j2
    make clean
@@ -42,11 +54,9 @@ fi
 
 # Build and run tests here
 
-echo "____________________________________"
-echo "............Deploy.................."
-echo "____________________________________"
+echo "Deploy..."
 
-# Package
+# Package 
 echo "Packaging ..."
 ls -al ${build_dir}
 tar -czvf  ${script_dir}/${ZIP_FILE_NAME} ${build_dir}
@@ -56,7 +66,7 @@ echo "Deploy is done"
 #echo "Deploy to Releases"
 #chmod +x ${script_dir}/push_github.sh
 
-# $GH_API_TOKEN comes from travis.yml env
+# $GH_API_TOKEN comes from travis.yml env  
 
 #GH_TAG=${TRAVIS_TAG}
 #${script_dir}/push_github.sh github_api_token=$GH_API_TOKEN owner_repo=$TRAVIS_REPO_SLUG tag=$GH_TAG filename=${script_dir}/${ZIP_FILE_NAME}
