13908e6b2c6d3850b7ffc05763bb1ec2bd6361e3
Pavel Kokolemin <pavelkok@gmail.com>
2019-10-26 11:16:19 +0300

    Update names

--- UnitTests/TestAddress.cpp
+++ UnitTests/TestAddress.cpp
@@ -2,12 +2,12 @@
 
 #include "Address.h"
 
-TEST(TestAddress, TestResolverSerialize)
+TEST(TestAddress, FromRecipientNestedSW)
 {
    auto addr = bs::Address("2NB6rBycxtMY2GxP8Swfc1VEzGigzbRDHnt");
-   auto resolver = addr.getRecipient(bs::XBTAmount(1.));
-   BinaryData script = resolver->getSerializedScript();
-   auto resolver2 = ScriptRecipient::deserialize(script);
-   auto addr2 = bs::Address::fromRecipient(resolver2);
+   auto recipient = addr.getRecipient(bs::XBTAmount(1.));
+   BinaryData script = recipient->getSerializedScript();
+   auto recipient2 = ScriptRecipient::deserialize(script);
+   auto addr2 = bs::Address::fromRecipient(recipient2);
    ASSERT_EQ(addr.display(), addr2);
 }
