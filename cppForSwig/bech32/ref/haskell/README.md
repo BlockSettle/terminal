520463404e3994c021c2a53c7a82831f5f10878a
Pavel Kokolemin <pavelkok@gmail.com>
2019-11-11 19:57:44 +0300

    Switch cppForSwig to submodule (#1602)
    * Switch cppForSwig to submodule
    
    * Windows compile fixes
    
    * Compile fixes
    
    * Move ArmoryDB to upper dir
    
    * Compile fix

--- cppForSwig/bech32/ref/haskell/README.md
+++
@@ -1,6 +0,0 @@
-To build and run the tests, you need [stack](https://docs.haskellstack.org/en/stable/README/).
-
-```sh
-$ stack setup # one time only
-$ stack test
-```
--- cppForSwig/leveldb_windows_port/win32_posix/sys/time.h
+++
@@ -1,3 +0,0 @@
-/***Empty file to stop the compiler from throwing errors about absent header files.
-I picked this option over commenting out these particular includes in order to speed up the upgrading process
-***/
