141a12c1929dbdf1d182a0299e4fe4bc8a3dbc6c
Roman Yurkovskiy <roman.yurkovskiy@gmail.com>
2020-01-21 09:54:14 +0200

    Add new license text for all files

--- BlockSettleUILib/ChatUI/ChatSearchListViewItemStyle.h
+++ BlockSettleUILib/ChatUI/ChatSearchListViewItemStyle.h
@@ -1,7 +1,7 @@
 /*
 
 ***********************************************************************************
-* Copyright (C) 2016 - 2019, BlockSettle AB
+* Copyright (C) 2016 - , BlockSettle AB
 * Distributed under the GNU Affero General Public License (AGPL v3)
 * See LICENSE or http://www.gnu.org/licenses/agpl.html
 *
