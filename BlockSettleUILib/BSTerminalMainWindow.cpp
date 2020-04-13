74ee9ddeea8e3320aa65611cce353ebe695c5107
Pavel Kokolemin <pavelkok@gmail.com>
2020-04-13 17:32:41 +0300

    Update coming soon text

--- BlockSettleUILib/BSTerminalMainWindow.cpp
+++ BlockSettleUILib/BSTerminalMainWindow.cpp
@@ -1345,7 +1345,7 @@ void BSTerminalMainWindow::onLoginProceed(const NetworkSettings &networkSettings
 {
    if (networkSettings.status == Blocksettle::Communication::GetNetworkSettingsResponse_Status_LIVE_TRADING_COMING_SOON) {
       BSMessageBox mbox(BSMessageBox::question, tr("Login to BlockSettle"), tr("Live trading is coming soon...")
-                   , tr("In the meantime, experience p2p trading in the testnet environment. Try out now?"), this);
+                   , tr("In the meantime, you can try p2p trading in our testnet environment. Would you like to do so now?"), this);
       mbox.setCancelButtonText(tr("Cancel"));
       mbox.setConfirmButtonText(tr("Yes"));
       int rc = mbox.exec();
