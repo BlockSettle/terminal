ff4bae9361f2727d369bfcccbada395fe7bf9e39
Ation <gation@gmail.com>
2021-04-05 18:29:52 +0300

    Update copy years according to commit history

--- BlockSettleSigner/EasyEncValidator.cpp
+++ BlockSettleSigner/EasyEncValidator.cpp
@@ -1,7 +1,7 @@
 /*
 
 ***********************************************************************************
-* Copyright (C) 2018 - 2020, BlockSettle AB
+* Copyright (C) 2020, BlockSettle AB
 * Distributed under the GNU Affero General Public License (AGPL v3)
 * See LICENSE or http://www.gnu.org/licenses/agpl.html
 *
