53f5cfcb2601e8a414696c056b4ee4b99427e658
nikitaromanenko99 <nikitaromanenko99@gmail.com>
2023-03-20 08:50:19 +0200

    [QT-37,38,41] update splash screen and icons

--- BlockSettleUILib/BSTerminalSplashScreen.cpp
+++ BlockSettleUILib/BSTerminalSplashScreen.cpp
@@ -23,23 +23,25 @@ BSTerminalSplashScreen::BSTerminalSplashScreen(const QPixmap& splash_image)
    progress_->setMinimum(0);
    progress_->setMaximum(100);
    progress_->setValue(0);
-   progress_->setMinimumWidth(this->width());
-   progress_->setMaximumHeight(10);
+   progress_->setMinimumWidth(this->width() - 10);
+   progress_->setMaximumHeight(8);
+   progress_->move(5, 289);
 
-   blockSettleLabel_ = new QLabel(this);
-   blockSettleLabel_->setText(QLatin1String("BLOCKSETTLE TERMINAL"));
-   blockSettleLabel_->move(30, 140);
-   blockSettleLabel_->setStyleSheet(QLatin1String("font-size: 18px; color: white"));
+   //blockSettleLabel_ = new QLabel(this);
+   //blockSettleLabel_->setText(QLatin1String("BLOCKSETTLE TERMINAL"));
+   //blockSettleLabel_->move(30, 140);
+   //blockSettleLabel_->setStyleSheet(QLatin1String("font-size: 18px; color: white"));
 
-   progress_->setStyleSheet(QLatin1String("text-align: center; font-size: 8px; border-width: 0px;"));
-   SetTipText(tr("Loading"));
+   progress_->setStyleSheet(QLatin1String("QProgressBar::chunk{background-color:#45A6FF}; background:#191E2A; text-align: center; font-size: 8px; border-width: 1px; border-color: #3C435A"));
+   //SetTipText(tr("Loading"));
+   progress_->setFormat(QString::fromStdString(""));
 }
 
 BSTerminalSplashScreen::~BSTerminalSplashScreen() = default;
 
 void BSTerminalSplashScreen::SetTipText(const QString& tip)
 {
-   progress_->setFormat(tip + QString::fromStdString(" %p%"));
+   //progress_->setFormat(tip + QString::fromStdString(" %p%"));
 //   QApplication::processEvents();
 }
 
--- GUI/QtQuick/QtQuickAdapter.h
+++ GUI/QtQuick/QtQuickAdapter.h
@@ -136,6 +136,9 @@ public:
    Q_PROPERTY(int armoryState READ armoryState NOTIFY armoryStateChanged)
    int armoryState() const { return armoryState_; }
 
+   Q_PROPERTY(int armoryServerIndex READ armoryServerIndex NOTIFY armoryServerIndexChanged)
+   int armoryServerIndex() const { return armoryServerIndex_; }
+
    Q_PROPERTY(HwDeviceModel* devices READ devices NOTIFY devicesChanged)
    HwDeviceModel* devices();
    Q_PROPERTY(bool scanningDevices READ scanningDevices NOTIFY scanningChanged)
@@ -190,6 +193,7 @@ signals:
    void addressGenerated();
    void settingChanged();
    void armoryStateChanged();
+   void armoryServerIndexChanged();
    void devicesChanged();
    void scanningChanged();
    void invokePINentry();
@@ -272,6 +276,7 @@ private:
    std::set<int>  createdComponents_;
    std::set<int>  loadingComponents_;
    int         armoryState_{ -1 };
+   int         armoryServerIndex_{ -1 };
    uint32_t    blockNum_{ 0 };
    int         signerState_{ -1 };
    std::string signerDetails_;
